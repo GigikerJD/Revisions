946cd7d0d9fe38cb7d995dd14df0f8296d9c4ef8
GigikerJD <nelgie.ferrol94@gmail.com>
2023-02-20 15:58:50 +0000

    adding TP4 package

---
+++ src/TP4/Beginner.java
@@ -0,0 +1,13 @@
+package TP4;
+
+public class Beginner {
+
+    private static int factRec(int n){
+        if(n == 1) return 1;
+        else return n * factRec(n-1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
