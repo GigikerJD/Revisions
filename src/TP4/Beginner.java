f777fda79b8640c7ec2d25d07cc2ede9f1756135
GigikerJD <nelgie.ferrol94@gmail.com>
2023-02-23 22:13:30 +0000

    Slight updates

--- src/TP4/Beginner.java
+++ src/TP4/Beginner.java
@@ -4,40 +4,40 @@ import java.util.Scanner;
 
 public class Beginner {
 
-    private static int factRec(int n){
+    public static int factRec(int n){
         if(n == 1) return 1;
         else return n * factRec(n-1);
     }
 
-    private static int fibonacciRec(int n){
+    public static int fibonacciRec(int n){
         if(n == 0) return 0;
         else if(n == 1) return 1;
         else return fibonacciRec(n-1) + fibonacciRec(n-2);
     }
 
-    private static int puissanceNonTerminale(int x, int n){
+    public static int puissanceNonTerminale(int x, int n){
         if(n == 0) return 1;
         else return x * puissanceNonTerminale(x, n-1);
     }
 
-    private static int gcd(int a, int b){
+    public static int gcd(int a, int b){
         if(a == b) return a;
         else if(b > a) return gcd(a, b-a);
         else return gcd(a-b, b);
     }
 
-    private static String DecToBinary(int nbr){
+    public static String DecToBinary(int nbr){
         if(nbr == 1) return "1";
         else if(nbr == 0) return "0";
         else return DecToBinary(nbr/2) + nbr%2;
     }
 
-    private static int nbDigits(int n){
+    public static int nbDigits(int n){
         if(n < 10) return 1;
         else return 1 + nbDigits(n/10);
     }
 
-    private static int sumDigits(int n){
+    public static int sumDigits(int n){
         if(n < 10) return n;
         else return n%10 + sumDigits(n/10);
     }
@@ -51,16 +51,54 @@ public class Beginner {
 
         while(true){
             switch (exercise){
+                case 1:
+                    System.out.print("Enter a random natural number > ");
+                    int number = key.nextInt();
+                    System.out.println("Factorial of " + number + " is " + factRec(number));
+                    break;
 
-            }
-            System.out.print("Do you want to continue ? y/o > ");
-            String anwser = key.next();
-            if(answer.charAt(0) == 'y'){
-                System.out.print("Which exercise ? > ");
-                exercise = key.nextInt();
-            }else {
-                System.out.println("Au revoir ! ");
-                break;
+                case 2:
+                    System.out.print("Enter a random natural number > ");
+                    number = key.nextInt();
+                    System.out.println("Fibonacci's result of " + number + " is " + fibonacciRec(number));
+                    break;
+
+                case 3:
+                    System.out.print("Enter a random integer > ");
+                    int a = key.nextInt();
+                    System.out.print("Enter a random power to raise > ");
+                    int n = key.nextInt();
+                    System.out.println(a + " raised to the power of " + n + " is " + puissanceNonTerminale(a, n));
+                    break;
+
+                case 4:
+                    System.out.print("Enter 1st value > ");
+                    int nb1 = key.nextInt();
+                    System.out.print("Enter 2nd value > ");
+                    int nb2 = key.nextInt();
+                    System.out.println("GCD of " + nb1 + " and " + nb2 + " is " + gcd(nb1, nb2));
+                    break;
+
+                case 5:
+                    System.out.print("Choose a number to convert in binary > ");
+                    int binary = key.nextInt();
+                    System.out.println("Binary converison of " + binary + " is " + DecToBinary(binary));
+                    break;
+
+                case 6:
+                    System.out.print("Choose an integer > ");
+                    int n1 = key.nextInt();
+                    System.out.println("Number of digits for " + n1 + " is " + nbDigits(n1));
+                    break;
+
+                case 7:
+                    System.out.print("Choose an integer > ");
+                    int n2 = key.nextInt();
+                    System.out.println("Sum of digits for " + n2 + " is " + sumDigits(n2));
+                    break;
+
+                default:
+                    System.err.println("Exercise number does not exist !!!");
             }
         }
     }
